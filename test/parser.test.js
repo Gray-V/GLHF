e27c3ececf70ee29c0cf6d2c818203644c045923
Aiden McDougald <aidenmcdougald@gmail.com>
2024-02-20 12:41:37 -0800

    fixed parser tests

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,8 +1,13 @@
 import parser from "../src/parser.js"
 import * as fs from "node:fs"
-import assert from "assert"
 
-assert.notEqual(parser(fs.readFileSync("examples/glhfProject/main.gg")), Error)
-assert.notEqual(parser(fs.readFileSync("examples/math.gg")), Error)
-assert.notEqual(parser(fs.readFileSync("examples/array.gg")), Error)
-assert.notEqual(parser(fs.readFileSync("examples/dictionary.gg")), Error)
+try {
+    parser(fs.readFileSync("examples/glhfProject/main.gg"))
+    parser(fs.readFileSync("examples/math.gg"))
+    parser(fs.readFileSync("examples/array.gg"))
+    parser(fs.readFileSync("examples/dictionary.gg"))
+    console.log("Syntax ok")
+} catch(e) {
+    console.log(e.message)
+    process.exit(1)
+}

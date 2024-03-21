75aaeb400e261a2533cdd8e2961cdc1bf5133b4d
Relectrik <viv@dluvs.com>
2024-03-20 20:42:16 -0700

    starting on static analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,3 +1,33 @@
-export default function analyze() {
-    throw new Error("Not yet implemented")
+import * as core from "./core.js"
+
+
+const INT = core.intType
+const FLOAT = core.floatType
+const STRING = core.stringType
+const BOOLEAN = core.boolType
+const ANY = core.anyType
+const VOID = core.voidType
+
+
+class Context {
+  constructor({ parent = null, locals = new Map(), inLoop = false, function: f = null }) {
+    Object.assign(this, { parent, locals, inLoop, function: f })
   }
+  add(name, entity) {
+    this.locals.set(name, entity)
+  }
+  lookup(name) {
+    return this.locals.get(name) || this.parent?.lookup(name)
+  }
+  static root() {
+    return new Context({ locals: new Map(Object.entries(core.standardLibrary)) })
+  }
+  newChildContext(props) {
+    return new Context({ ...this, ...props, parent: this, locals: new Map() })
+  }
+}
+
+export default function analyze(match) {
+
+
+}
